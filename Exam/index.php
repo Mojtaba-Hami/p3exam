d667449ac2938c696a998133fb02abb4afc216cb
Mojtaba-Hami <mrmojih@gmail.com>
2025-02-13 10:06:54 +0330

    basic structure of the login form

---
+++ Exam/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <h1></h1>
+    <div class="container">
+        <h3>Login</h3>
+
+        <div class="inputs">
+        <input type="text" placeholder="Fullname" class="input">
+        <p></p>
+        <input type="text" placeholder="Email" class="input">
+        <p></p>
+        <input type="text" placeholder="Password" class="input">
+        <p></p>
+        <input type="text"placeholder="rewrite password" class="input">
+        <p></p>
+        <button>submit</button>
+        </div>
+
+    </div>
+
+    <script src="./app.js"></script>
+</body>
+</html>
