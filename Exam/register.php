97a736fc6bac5b41a31d3e1bbc498246ffb51387
Mojtaba-Hami <mrmojih@gmail.com>
2025-02-14 12:26:15 +0330

    the styles of register page

--- Exam/register.php
+++ Exam/register.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./register.css">
 </head>
 <body>
 <?php 
@@ -12,28 +13,35 @@
     $email = $_POST['email'];
     $password = $_POST['password'];
     $repassword = $_POST['repassword'];
-
-    echo "Fullname: " . $fullname . "<br>";
-    echo "Email: " . $email . "<br>";
-    echo "Password: " . $password . "<br>";
-    echo "Repassword: " . $repassword . "<br>";
-
-   if (!empty($fullname) && !empty($email) && !empty($password) && !empty($repassword)) {
+?>
+    
+<?php
+    if (!empty($fullname) && !empty($email) && !empty($password) && !empty($repassword)) {
     if ($password === $repassword) {
         if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "<div class='content'>";
             echo "<h1>ورورد موفقیت آمیز بود</h1>";
+            echo "<img src='./success-icon-10.png'>";
+            echo "</div>";
         } else {
+            echo "<div class='content'>";
             echo "<h1>فرمت ایمیل وارد شده صحیح نمی‌باشد</h1>";
+            echo "   <img src='./pngtree-failed-rubber-stamp-failure-png-image_9941934.png' >";
+            echo "</div>";
         }
-     } else {
+    } else {
+        echo "<div class='content'>";
         echo "<h1>پسورد و تکرار آن یکسان نیستند</h1>";
+        echo "   <img src='./pngtree-failed-rubber-stamp-failure-png-image_9941934.png' >";
+        echo "</div>";
     }
 } else {
+    echo "<div class='content'>";
     echo "<h1>لطفا تمامی فیلدها را وارد کنید!</h1>";
+    echo "<img src='./pngtree-failed-rubber-stamp-failure-png-image_9941934.png' >";
+    echo "</div>";
 }
-
-    ?>
+?>
     
-   
 </body>
 </html>
